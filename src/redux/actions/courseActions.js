da125939f73d27f0dd160e64f9e4c340844a5ae5
marcosTulli <marcostulli@hotmail.com>
2021-11-14 19:16:35 +0200

    fixSaveButton

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,16 +1,16 @@
 import * as types from "./actionTypes";
-import * as courseApi from "../../api/courseApi"
+import * as courseApi from "../../api/courseApi";
 
 export function loadCourseSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
-export function updateCourseSuccess(courses) {
-    return { type: types.UPDATE_COURSES_SUCCESS, courses };
+export function createCourseSuccess(course) {
+    return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
-export function createCourseSuccess(courses) {
-    return { type: types.CREATE_COURSES_SUCCESS, courses };
+export function updateCourseSuccess(course) {
+    return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -18,16 +18,17 @@ export function loadCourses() {
         return courseApi
             .getCourses()
             .then(courses => {
-                dispatch(loadCourseSuccess(courses))
-            }).catch(error => {
-                throw error;
+                dispatch(loadCourseSuccess(courses));
             })
-    }
+            .catch(error => {
+                throw error;
+            });
+    };
 }
 
 export function saveCourse(course) {
-    // eslint-disable-next-line no-unused-vars
-    return function (dispatch) {
+    //eslint-disable-next-line no-unused-vars
+    return function (dispatch, getState) {
         return courseApi
             .saveCourse(course)
             .then(savedCourse => {
@@ -37,7 +38,6 @@ export function saveCourse(course) {
             })
             .catch(error => {
                 throw error;
-            })
-    }
-
+            });
+    };
 }
