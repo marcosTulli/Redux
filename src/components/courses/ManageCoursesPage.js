8d66b95859b1a850a9e915773c05ac4a5106c9f9
marcosTulli <marcostulli@hotmail.com>
2021-11-14 19:11:47 +0200

    savebuttonerror

--- src/components/courses/ManageCoursesPage.js
+++ src/components/courses/ManageCoursesPage.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from './CourseForm'
 import { newCourse } from "../../../tools/mockData"
+
 
 function ManageCoursePage({
     courses,
     authors,
     loadAuthors,
     loadCourses,
+    saveCourse,
+    history,
     ...props
 }) {
 
     const [course, setCourse] = useState({ ...props.course });
-    const [errors, setErrors] = useState();
+    const [errors, setErrors] = useState({});
 
     useEffect(() => {
         if (courses.length === 0) {
@@ -31,12 +34,29 @@ function ManageCoursePage({
         }
     }, []);
 
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setCourse(prevCourse => ({
+            ...prevCourse,
+            [name]: name === "authorId" ? parseInt(value, 10) : value
+        }));
+    }
+
+    function handleSave(event) {
+        event.preventDefault();
+        saveCourse(course).then(() => {
+            history.push('/courses')
+        });
+    }
+
     return (
 
         <CourseForm
             course={course}
             errors={errors}
-            authors={authors} />
+            authors={authors}
+            onChange={handleChange}
+            onSave={handleSave} />
     );
 }
 
@@ -46,7 +66,10 @@ ManageCoursePage.propTypes = {
     authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
     loadCourses: PropTypes.func.isRequired,
-    loadAuthors: PropTypes.func.isRequired
+    loadAuthors: PropTypes.func.isRequired,
+    saveCourse: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired
+
 };
 
 function mapStateToProps(state) {
@@ -59,7 +82,8 @@ function mapStateToProps(state) {
 
 const mapDispatchToProps = {
     loadCourses,
-    loadAuthors
+    loadAuthors,
+    saveCourse
 }
 
 export default connect(
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,23 +1,43 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi"
 
-export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course };
-}
-
-export function loadCoursesSuccess(courses) {
+export function loadCourseSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function updateCourseSuccess(courses) {
+    return { type: types.UPDATE_COURSES_SUCCESS, courses };
+}
+
+export function createCourseSuccess(courses) {
+    return { type: types.CREATE_COURSES_SUCCESS, courses };
+}
 
 export function loadCourses() {
     return function (dispatch) {
         return courseApi
             .getCourses()
             .then(courses => {
-                dispatch(loadCoursesSuccess(courses))
+                dispatch(loadCourseSuccess(courses))
             }).catch(error => {
                 throw error;
             })
     }
+}
+
+export function saveCourse(course) {
+    // eslint-disable-next-line no-unused-vars
+    return function (dispatch) {
+        return courseApi
+            .saveCourse(course)
+            .then(savedCourse => {
+                course.id
+                    ? dispatch(updateCourseSuccess(savedCourse))
+                    : dispatch(createCourseSuccess(savedCourse));
+            })
+            .catch(error => {
+                throw error;
+            })
+    }
+
 }
